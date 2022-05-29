9aaa90d63c91d011e97a03d9832f0886ebcc4dde
Farzan-I <farzan_15@hotmail.co.uk>
2022-05-29 20:52:10 +0100

    Need to sort the getTotal method

--- spec/order.test.js
+++ spec/order.test.js
@@ -4,7 +4,14 @@ describe('Order', () => {
   it('adds a dish to the basket', () => {
     var order = new Order();
     order.pickDish("meatball sub");
-    expect(order.viewBasket()).toEqual(["meatball sub"])
+    expect(order.viewBasket()).toEqual(["meatball sub"]);
   })
+
+  it('calculates total sum of the order', () => {
+    var order = new Order();
+    order.pickDish("meatball sub");
+    expect(order.getTotal()).toEqual("Your basket's total is £5.");
+  })
+
 })
 
